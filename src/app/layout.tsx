73bda08d105eea0667f62c222af009f7ad9e5b79
7johnsz <contatoerro708@gmail.com>
2025-07-25 17:34:19 -0300

    ✨ feat(metadata): enhance SEO metadata
    - add keywords for better search visibility
    - include author and creator information
    - configure open graph metadata for social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,25 @@ const geistSans = localFont({
 export const metadata: Metadata = {
   title: "João Victor | Devjohn",
   description: "Seja bem-vindo(a) ao meu mundo!",
+  keywords: ["João Victor", "Desenvolvedor", "Designer", "UX/UI", "Portfolio", "Devjohn", "Desenvolvimento Web", "Design de Interfaces"],
+  authors: [{ name: "João Victor", url: "https://devjohn.com.br" }],
+  creator: "João Victor",
+  openGraph: {
+    title: "João Victor | Devjohn",
+    description: "Seja bem-vindo(a) ao meu mundo!",
+    url: "https://devjohn.com.br",
+    siteName: "Devjohn",
+    // images: [
+    //   {
+    //     url: "https://devjohn.com.br/",
+    //     width: 1200,
+    //     height: 630,
+    //     alt: "João Victor | Devjohn",
+    //   },
+    // ],
+    locale: "pt-BR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
